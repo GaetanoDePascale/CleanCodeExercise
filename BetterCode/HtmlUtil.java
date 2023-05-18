4678b83f1f1f8e84ff30e1b3cb635fea8b086dd3
g.depascale <g.depascale@nexsoft.it>
2023-05-18 22:07:06 +0200

    add

---
+++ BetterCode/HtmlUtil.java
@@ -0,0 +1,16 @@
+package BetterCode;
+
+public class HtmlUtil {
+    public static String renderPageWithSetupsAndTeardowns(PageData pageData, boolean isSuite) throws Exception {
+        boolean isTestPage = pageData.hasAttribute("Test");
+        if (isTestPage) {
+            WikiPage testPage = pageData.getWikiPage();
+            StringBuffer newPageContent = new StringBuffer();
+            includeSetupPages(testPage, newPageContent, isSuite);
+            newPageContent.append(pageData.getContent());
+            includeTeardownPages(testPage, newPageContent, isSuite);
+            pageData.setContent(newPageContent.toString());
+        }
+        return pageData.getHtml();
+    }
+}
---
+++ BetterCode/OpenClosed.cs
@@ -0,0 +1,36 @@
+// Better Code
+interface IAdapter
+{
+    bool Request(string url);
+}
+
+class AjaxAdapter : IAdapter
+{
+    public bool Request(string url)
+    {
+        // request and return promise
+    }
+}
+
+class NodeAdapter : IAdapter
+{
+    public bool Request(string url)
+    {
+        // request and return promise
+    }
+}
+
+class HttpRequester
+{
+    private readonly IAdapter Adapter;
+
+    public HttpRequester(IAdapter adapter)
+    {
+        Adapter = adapter;
+    }
+
+    public bool Fetch(string url)
+    {
+        return Adapter.Request(url);
+    }
+}
